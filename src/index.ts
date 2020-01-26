677961d643398d929abd9b69550b95991a5d412b
lollipop-onl <lollipop@simoochee.net>
2020-01-26 18:04:51 +0900

    ✨ Add ESLint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 /**
  * noop function
  */
-const noop = (): void => {};
+const noop = (): void => {
+  // do nothing
+};
 
 /**
  * noop function (lax types)
  * @param args accept any arguments
  */
-const noopLax = (...args: any[]): any => {};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+const noopLax = (...args: any[]): any => {
+  // do nothing
+};
 
 export { noopLax };
 export default noop;
